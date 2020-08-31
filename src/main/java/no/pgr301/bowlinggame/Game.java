906fde46cb064d3099e2982400bb524d9be99390
jonerikrotnebo <jonrotnebo1@gmail.com>
2020-08-31 10:35:59 +0200

    added code, missing test

--- src/main/java/no/pgr301/bowlinggame/Game.java
+++ src/main/java/no/pgr301/bowlinggame/Game.java
@@ -2,16 +2,51 @@ package no.pgr301.bowlinggame;
 
 public class Game {
 
+    private int rolls[] = new int[21];
+    private int currenRoll = 0;
+
     public void roll(int pins) {
-        //test
-        //123
+        rolls[currenRoll++]= pins;
     }
 
     public int score() {
-        return 0;
+
+        int score = 0;
+        int frameIndex= 0;
+        for (int frame = 0; frame <10; frame++){
+            if (isStrike(frameIndex)) {
+                score += 10 + strikeBonus(frameIndex);
+                frameIndex++;
+            }else if (isSpear(frameIndex)) {
+                score +=10 + spareBonus(frameIndex);
+                frameIndex+=2;
+            } else {
+                score += sumOfBallsInFrame(frameIndex);
+                frameIndex+=2;
+            }
+        }
+
+        return score;
     }
 
-    //tes
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
+    }
 
+    private int sumOfBallsInFrame(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex+1];
+    }
+
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex+2];
+    }
+
+    private int strikeBonus(int frameIndex) {
+        return rolls[frameIndex+1] + rolls[frameIndex+2];
+    }
+
+    private boolean isSpear(int frameIndex) {
+        return rolls[frameIndex]+rolls[frameIndex+1] == 10;
+    }
 
 }
